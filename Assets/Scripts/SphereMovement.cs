9e26222707e75d5b1d6a1000e0fc668a29b656fd
Kirik <95646707+1Kirik3@users.noreply.github.com>
2026-01-12 21:43:14 +0300

    project structure reorganiztion

--- Assets/Scripts/SphereMovement.cs
+++ Assets/Scripts/SphereMovement.cs
@@ -1,13 +1,13 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SphereMovement : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
